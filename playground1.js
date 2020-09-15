29fda6372f6f309efd6b142e4da46ed70ef33472
mich <mich-cook@users.noreply.github.com>
2020-09-15 15:33:37 -0700

    adding a few properties that have special cases so they're readily available for review

--- playground1.js
+++ playground1.js
@@ -1,6 +1,11 @@
 ReactDOM.render(
   // React.DOM.h1() in the book appears to be dead
-  React.createElement("div", { "id": "top" }, `The Heading`, React.createElement("h1", null, `Should contain an h1`)),
+  React.createElement(
+    "div",
+    { "id": "top", "className": "containers", "style": { "fontWeight": "bold" }},
+    `The Heading`,
+    React.createElement("h1", null, `Should contain an h1`)
+  ),
   document.getElementById("react-app")
 );
 
