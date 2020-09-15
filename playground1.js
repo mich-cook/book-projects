ea179039b8d47735683b9ce4ec23ca4e59c5ca8b
mich <mich-cook@users.noreply.github.com>
2020-09-15 16:53:03 -0700

    adding trivial proptypes to playground 1 which requires another unpkg script

--- playground1.js
+++ playground1.js
@@ -4,6 +4,17 @@ const Subtitle = React.memo(props =>
   React.createElement("h2", null, `And could have a subheading, ${props.name}`)
 );
 
+// uses an extra JS for this legacy stub so we can do propTypes
+// as shown. other ways likely to follow.
+const Description = createReactClass({
+  "propTypes": { "color": PropTypes.string.isRequired },
+  "getDefaultProps": function() { return { "color": "neon orange" }; },
+  "render": function() {
+    return React.createElement("p", null, `Without these, we might be ${this.props.color}`);
+  }
+});
+// list of all propTypes also in Object.keys(PropTypes)
+
 ReactDOM.render(
   // React.DOM.h1() in the book appears to be dead
   React.createElement(
@@ -11,7 +22,8 @@ ReactDOM.render(
     { "id": "top", "className": "containers", "style": { "fontWeight": "bold" }},
     `The Heading`,
     React.createElement("h1", null, `Should contain an h1`),
-    React.createElement(Subtitle, { "name": "Boberick" })
+    React.createElement(Subtitle, { "name": "Boberick" }),
+    React.createElement(Description) // , { "color": "lime green" })
   ),
   document.getElementById("react-app")
 );
