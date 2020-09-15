165d88d784ce1ebf81a3fcd3cdf1af347bb97d7b
mich <mich-cook@users.noreply.github.com>
2020-09-15 15:28:25 -0700

    adding properties for the DOM element and nesting another inside

--- playground1.js
+++ playground1.js
@@ -1,5 +1,7 @@
 ReactDOM.render(
   // React.DOM.h1() in the book appears to be dead
-  React.createElement("div", null, "Heading1"),
+  React.createElement("div", { "id": "top" }, `The Heading`, React.createElement("h1", null, `Should contain an h1`)),
   document.getElementById("react-app")
 );
+
+// plenty more toys in Object.keys(React) and Object.keys(ReactDOM)
