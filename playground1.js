7c79227a9dc7fcdf6cf8d831860ac11b46eb2ac5
mich <mich-cook@users.noreply.github.com>
2020-09-15 16:30:36 -0700

    add subheading component using the only React method left standing since the book was written

--- playground1.js
+++ playground1.js
@@ -1,10 +1,17 @@
+// NOTE: React.createClass() no longer exists
+// NOTE: React.createFactory() is considered legacy and disuse is recommended
+const Subtitle = React.memo(props =>
+  React.createElement("h2", null, `And could have a subheading, ${props.name}`)
+);
+
 ReactDOM.render(
   // React.DOM.h1() in the book appears to be dead
   React.createElement(
     "div",
     { "id": "top", "className": "containers", "style": { "fontWeight": "bold" }},
     `The Heading`,
-    React.createElement("h1", null, `Should contain an h1`)
+    React.createElement("h1", null, `Should contain an h1`),
+    React.createElement(Subtitle, { "name": "Boberick" })
   ),
   document.getElementById("react-app")
 );
