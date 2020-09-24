26bc5175027a1b648ad6d7934cbbef4220e6db99
mich <mich-cook@users.noreply.github.com>
2020-09-24 16:37:33 -0700

    add some sample data and display it in the table body

--- playground2.js
+++ playground2.js
@@ -1,19 +1,51 @@
 const headers = [ "mission", "shuttle", "date" ];
-const data = {};
+const data = [
+{
+  "mission": "1",
+  "shuttle": "Challenger",
+  "date": "6"
+},
+{
+  "mission": "2",
+  "shuttle": "Enterprise",
+  "date": "7"
+},
+{
+  "mission": "3",
+  "shuttle": "Atlantis",
+  "date": "8"
+}
+];
 
 const Table = createReactClass({
+
+  "getInitialState": function() {
+    return { "data": this.props.initialData };
+  },
+
   "render": function() {
     return (
       React.createElement("table", null,
         React.createElement("thead", null,
           React.createElement("tr", null, this.props.headers.map(function(heading, i) {
             return React.createElement("th", { "key": i }, heading);
-      }))))
+          }))
+        ),
+        React.createElement("tbody", null, this.state.data.map(function(row, i) {
+          return (
+            React.createElement("tr", null, [
+              React.createElement("td", null, row.mission),
+              React.createElement("td", null, row.shuttle),
+              React.createElement("td", null, row.date)
+            ])
+          );
+        }))
+      )
     );
   }
 });
 
 ReactDOM.render(
-  React.createElement(Table, { headers, data }),
+  React.createElement(Table, { headers, "initialData": data }),
   document.getElementById("react-app")
 );
