75021591f5cb97e4d4ebaab83f19f31da1b4c0cf
mich <mich-cook@users.noreply.github.com>
2020-09-29 00:13:18 -0700

    adding basic app implementation that will use hand rolled build process

---
+++ playground4/js/source/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo.js';
+
+ReactDOM.render(
+  <h1><Logo />Hello World!</h1>,
+  document.getElementById('react-app')
+);
---
+++ playground4/js/source/components/Logo.js
@@ -0,0 +1,10 @@
+//import createReactClass from 'create-react-class';
+import React from 'react';
+
+class Logo extends React.Component {
+  render() {
+    return <div className="Logo"></div>;
+  }
+}
+
+export default Logo;
