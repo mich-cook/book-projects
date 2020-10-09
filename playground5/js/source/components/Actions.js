d5861ead32857ced847ca7b73749dfec4dd1e776
mich <mich-cook@users.noreply.github.com>
2020-10-09 15:10:12 -0700

    remove edit and delete buttons from actions right now since they won't actually affect the stored data. may be revisited for creating a tailored exportable dataset.

--- playground5/js/source/components/Actions.js
+++ playground5/js/source/components/Actions.js
@@ -4,10 +4,10 @@ const Actions = props =>
   <div className="Actions">
     <span tabIndex="0" className="ActionsInfo" title="More info"
       onClick={props.onAction.bind(null, 'info')}>&#8505;</span>
-    <span tabIndex="0" className="ActionsEdit" title="Edit"
+    {/*<span tabIndex="0" className="ActionsEdit" title="Edit"
       onClick={props.onAction.bind(null, 'edit')}>&#10000;</span>
     <span tabIndex="0" className="ActionsDelete" title="Delete"
-      onClick={props.onAction.bind(null, 'delete')}>X</span>
+      onClick={props.onAction.bind(null, 'delete')}>X</span>*/}
   </div>;
 
 /*
