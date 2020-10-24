7bed5809553a04490d5102ff074f44c09074febd
mich <mich-cook@users.noreply.github.com>
2020-10-23 23:08:28 -0700

    adding a flux store

---
+++ playground6/js/source/flux/Store.js
@@ -0,0 +1,59 @@
+// @flow
+
+let schema;
+let data = [
+{
+  "mission": "1",
+  "shuttle": "Challenger",
+  "date": "6"
+},
+{
+  "mission": "2",
+  "shuttle": "Enterprise",
+  "date": "7"
+},
+{
+  "mission": "3",
+  "shuttle": "Atlantis",
+  "date": "8"
+}
+];
+
+const headers = [ "mission", "shuttle", "date" ];
+
+const Store = {
+  getData(): Array<Object> {
+    return data;
+  },
+
+  setData(newDdata: Array<Object>, commit: boolean = true) {
+    data = newData;
+    if (commit === true) {
+      localStorage.setItem('data', JSON.stringify(data));
+    }
+  },
+
+  getCount(): number {
+    return data.length;
+  },
+
+  getSchema(): Array<Object> {
+    return schema;
+  },
+
+  getHeaders(): Array<Object> {
+    return headers;
+  },
+
+  init(initSchema: Array<Object>) {
+    schema = initSchema;
+
+    const storage = 'localStorage' in window ? localStorage.getItem('data') : null;
+    if (storage !== null) {
+      data = JSON.parse(storage);
+    }
+  }
+
+};
+
+export default Store;
