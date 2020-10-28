2085345ce4322cbcd5725bbf323214b2a1f49566
mich <mich-cook@users.noreply.github.com>
2020-10-28 01:26:44 -0700

    add filtering to Store Actions

--- playground6/js/source/flux/Actions.js
+++ playground6/js/source/flux/Actions.js
@@ -39,6 +39,40 @@ const Actions = {
     let data = FluxStore.getData();
     data.sort((a, b) => this._sortCallback(a[key], b[key], descending));
     FluxStore.setData(data);
+  },
+
+  _preFilterData: null,
+
+  filterStart() {
+    this._preFilterData = FluxStore.getData();
+  },
+
+  filterEnd() {
+    FluxStore.setData(this._preFilterData);
+  },
+
+  // TODO: Better signature would be just the needle and optional column
+  //   removing any required knowledge of the UI to handle the event object
+  filterData(e: Event) {
+    const needle = e.target.value.toLowerCase();
+    if (needle.length === 0) {
+      FluxStore.setData(this._preFilterData, false);
+      return;
+    }
+
+    const key = FluxStore.getHeaders();
+    if (!this._preFilterData) { return; }
+
+    const results = this._preFilterData.filter(mission => {
+      for (let f = 0; f < key.length; f += 1) {
+        if (mission[key[f]].toString().toLowerCase().includes(needle) === true) {
+          return true;
+        }
+      }
+      return false;
+    });
+
+    FluxStore.setData(results, false);
   }
 
 };
