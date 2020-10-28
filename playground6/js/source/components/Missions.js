25e0aa4f9c69392fc3b896541b4c3f4e0292e0df
mich <mich-cook@users.noreply.github.com>
2020-10-28 01:29:05 -0700

    implement Flux filter Actions in <Missions> to replace local filtering

--- playground6/js/source/components/Missions.js
+++ playground6/js/source/components/Missions.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import FluxStore from '../flux/Store.js';
+import FluxActions from '../flux/Actions.js';
 
 import Datatable from './Datatable.js';
 
@@ -9,15 +10,12 @@ class Missions extends Component {
   constructor(props) {
     super(props);
     this.state = {
-//      "data": props.initialData,
       "count": FluxStore.getCount()
     };
 
     FluxStore.addListener('change', () => {
       this.setState({ "count": FluxStore.getCount() });
     });
-
-    this.preFilterData = null;
   }
 
   shouldComponentUpdate(props: Object, state: State): boolean {
@@ -33,40 +31,14 @@ class Missions extends Component {
     localStorage.setItem('data', JSON.stringify(data));
   }
 
-  filterStart() {
-    this.preFilterData = FluxStore.getData(); // this.state.data;
-  }
-
-  filterEnd() {
-    this.setState({ "data": FluxStore.getData() /*this.preFilterData */ });
-  }
-
-  filterData(e) {
-    const needle = e.target.value.toLowerCase();
-    if (needle.length === 0) {
-      this.setState({ "data": this.preFilterData });
-      return;
-    }
-
-    const key = FluxStore.getHeaders(); // this.props.headers;  // improve to use this.props.schema.map(item => item.id); to match this.props.headers
-
-    const results = this.preFilterData.filter(mission => {
-      for (let f = 0; f < key.length; f += 1) {
-        if (mission[key[f]].toString().toLowerCase().includes(needle) === true) {
-          return true;
-        }
-      }
-      return false;
-    });
-
-    this.setState({ "data": results });
-  }
-
 //        <Datatable initialData={this.state.data} onDataChange={this.datatableDataChange.bind(this)} />
   render() {
     return (
       <div>
-        <input placeholder="Filter Missions" onChange={this.filterData.bind(this)} onFocus={this.filterStart.bind(this)} onBlur={this.filterEnd.bind(this)} />
+        <input placeholder="Filter Missions"
+          onChange={FluxActions.filterData.bind(FluxActions)}
+          onFocus={FluxActions.filterStart.bind(FluxActions)}
+          onBlur={FluxActions.filterEnd.bind(FluxActions)} />
         <Datatable />
       </div>
     );
